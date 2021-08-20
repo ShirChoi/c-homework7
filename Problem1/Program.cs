3f0340b4dafedfd8bf0c9f9a8c525e4242d49e9d
ShirChoi <abdulloevsarvar2005@gmail.com>
2021-08-20 11:34:39 +0500

    first commit

---
+++ Problem1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Problem1 {
+    class Program {
+        static void Main(string[] args) {
+            Rectangle rect1 = new Rectangle(ReadDouble("Rect1 side1"), ReadDouble("Rect1 side2"));
+
+            System.Console.WriteLine($"Rect1 perimeter: {rect1.Perimeter}");
+            System.Console.WriteLine($"Rect1 area: {rect1.Area}");
+
+            Rectangle rect2 = new Rectangle(ReadDouble("Rect2 side1"), ReadDouble("Rect2 side2"));
+
+            System.Console.WriteLine($"Rect2 perimeter: {rect2.Perimeter}");
+            System.Console.WriteLine($"Rect2 area: {rect2.Area}");
+        }
+
+        static double ReadDouble(string varName) {
+            System.Console.Write(varName + ": ");
+            return double.Parse(Console.ReadLine());
+        }
+    }
+
+    class Rectangle {
+        private double _side1, _side2;
+        public double Area {
+            get {
+                return AreaCalculator();
+            }
+        }
+
+        public double Perimeter {
+            get {
+                return PerimeterCalculator();
+            }
+        }
+
+        public Rectangle(double side1, double side2) {
+            _side1 = side1;
+            _side2 = side2;
+        }   
+
+        private double AreaCalculator() {
+            return _side1 * _side2;
+        }
+
+        private double PerimeterCalculator() {
+            return 2 * (_side1 + _side2);
+        }
+    }
+}
---
+++ Problem2/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace Problem2 {
+    class Program {
+        static void Main(string[] args) {
+            Book book1 = new Book {
+                Title = new Title(info: "Война и мир", color: ConsoleColor.Red),
+                Author = new Author(info: "Лев Толстой", color: ConsoleColor.Gray),
+                Content = new Content(info: "куча страниц интересного текста", color: ConsoleColor.Blue) 
+            };
+
+            book1.Show();
+
+            System.Console.WriteLine("---------------------------------------------");
+
+            Book book2 = new Book {
+                Title = new Title(info: "451 градус по Фаренгейту", color: ConsoleColor.Red),
+                Author = new Author(info: "Рэй Брэдбери", color: ConsoleColor.Gray),
+                Content = new Content(info: "пожарные создают пожары, а не тушат их", color: ConsoleColor.Blue) 
+            };
+
+            book2.Show();
+        }
+    }
+
+    class Book {
+        public Title Title { get; set; }
+        public Author Author { get; set; }
+        public Content Content { get; set; }
+
+        public void Show() {
+            Title?.Show();
+            Author?.Show();
+            Content?.Show();
+        }
+    }
+    class Title {
+        protected ConsoleColor _color;
+        protected string _info;
+        public Title() {}
+        public Title(string info, ConsoleColor color) {
+            _info = info;
+            _color = color;
+        }
+
+        public void Show() {
+            Console.ForegroundColor = _color;
+            Console.WriteLine($"Название: {_info}");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+    class Author {
+        protected ConsoleColor _color;
+        protected string _info;
+        public Author() {}
+        public Author(string info, ConsoleColor color) {
+            _info = info;
+            _color = color;
+        }
+
+        public void Show() {
+            Console.ForegroundColor = _color;
+            Console.WriteLine($"Автор: {_info}");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+    class Content {
+        protected ConsoleColor _color;
+        protected string _info;
+        public Content() {}
+        public Content(string info, ConsoleColor color) {
+            _info = info;
+            _color = color;
+        }
+
+        public void Show() {
+            Console.ForegroundColor = _color;
+            Console.WriteLine($"Содержание: {_info}");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
